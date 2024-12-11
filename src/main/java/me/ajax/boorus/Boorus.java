2fb12d3b68a1b97c0fb58e14910c6df8e7fb5095
ayaxdev <danielthegreat@disroot.org>
2024-12-11 17:46:01 +0100

    Added some more popular boorus and removed dead links I forgot to check

--- src/main/java/me/ajax/boorus/Boorus.java
+++ src/main/java/me/ajax/boorus/Boorus.java
@@ -6,8 +6,13 @@ public enum Boorus {
     GELBOORU("https://gelbooru.com/"),
     THE_BIG_IMAGEBOARD("https://tbib.org/"),
     MSPA("http://mspabooru.com/"),
-    PONI_BOORU("http://pbooru.com/"),
-    XBOORU("https://xbooru.com/");
+    XBOORU("https://xbooru.com/"),
+    THE_COLLECTION("https://the-collection.booru.org/"),
+    GARYCBOORU("https://garycbooru.booru.org/"),
+    ALL_GIRL("https://allgirl.booru.org/"),
+    THE_AZURE_BLADE("https://tab.booru.org/"),
+    ROZEN_MAIDEN("https://rm.booru.org/"),
+    DRAW_FRIENDS("https://drawfriends.booru.org/");
 
     public final String baseUrl;
 
